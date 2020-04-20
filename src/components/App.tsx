dd5fed27335526b8dbe9b78953209e28f862e4fe
Martin Davis <martin_davis3@yahoo.ca>
2020-04-20 17:47:14 -0600

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Fragment } from 'react';
+
+export interface IAppProps {
+}
+
+export default class App extends React.Component<IAppProps> {
+  public render() {
+    return (
+      <Fragment>
+        <h1>Hello, World!</h1>
+      </Fragment>
+    );
+  }
+}
