f9d7def067a9aa4e171f9b8588cf0ebeb0c63f59
Toyin <oluwatoyinogundare@gmail.com>
2020-04-22 23:07:01 -0600

    sign-up created

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Fragment } from 'react';
 import CommentsForm from './Comments';
+import Signup from './SIGN_UP';
 
 
 
@@ -12,6 +13,7 @@ export default class App extends React.Component<IAppProps> {
     return (
       <Fragment>
         {/* <h1>Hello, World!</h1> */}
+        <Signup/>
         <CommentsForm/>
       </Fragment>
     );
