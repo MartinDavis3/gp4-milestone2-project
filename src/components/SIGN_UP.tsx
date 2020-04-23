2b4f0add3f8f8eb25f290288a1d7b843e9621688
Toyin <oluwatoyinogundare@gmail.com>
2020-04-22 23:08:00 -0600

    text removed

--- src/components/SIGN_UP.tsx
+++ src/components/SIGN_UP.tsx
@@ -11,19 +11,7 @@ export default class Signup extends React.Component<ISignupProps> {
 
 <Grid.Column width={1}></Grid.Column>
 <Grid.Column width={5}>
-<Container fluid>
-<Header as='h2'>Dogs Roles with Humans</Header>
-<p>
-Domestic dogs inherited complex behaviors, such as bite inhibition, from
-their wolf ancestors, which would have been pack hunters with complex
-body language. These sophisticated forms of social cognition and
-communication may account for their trainability, playfulness, and
-ability to fit into human households and social situations, and these
-attributes have given dogs a relationship with humans that has enabled
-them to become one of the most successful species on the planet today.
-</p>
 
-</Container>
 </Grid.Column>
 <Grid.Column>      
 
