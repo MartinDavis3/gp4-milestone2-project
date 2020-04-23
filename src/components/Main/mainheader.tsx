809497de82a301037a805413cfff18485b572759
Martin Davis <martin_davis3@yahoo.ca>
2020-04-23 11:00:53 -0600

    removed second heading

--- src/components/Main/mainheader.tsx
+++ src/components/Main/mainheader.tsx
@@ -18,8 +18,6 @@ export default class Mainheader extends React.Component<ImMainheaderProps> {
             <Header as='h1' color='blue' textAlign='center' size='huge'>
               Welcome to our social media website
             </Header> 
-            <Header as='h2'>Lorem ipsum dolor sit amet
-            </Header>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vestibulum elit in tortor finibus, a iaculis enim eleifend. Duis a sollicitudin libero. Pellentesque non purus quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla semper tellus, nec gravida odio mollis maximus. Integer ac libero eros. Nam quis consequat justo. Sed sit amet accumsan odio. 
             </p>
