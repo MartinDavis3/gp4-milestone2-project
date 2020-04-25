337eca695f5ed0f3e94cfd3553c2307d252e4b71
Toyin <oluwatoyinogundare@gmail.com>
2020-04-25 04:29:31 -0600

    errors cleared

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,97 +1,96 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import * as React from 'react';
-import {RootState} from '../store';
-import  {signUp } from  '../store/user/actions';
-import {User} from '../store/user/types'
-import { Grid, Form, Button,  Divider, Header } from 'semantic-ui-react';
+import { RootState } from '../store';
+import { signUp } from '../store/user/actions';
+import { User } from '../store/user/types'
+import { Grid, Form, Button, Divider, Header } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
-export interface IRegisterProps
-{
-    signUp: typeof signUp,
-    userList: User[]
+export interface IRegisterProps {
+  signUp: typeof signUp,
+  userList: User[]
 }
 
-export class Register extends React.Component <IRegisterProps>
+export class Register extends React.Component<IRegisterProps>
 {
-        generateID = (): number => {
-        let randomNumber: number = Math.floor( Math.random() * 1000 );
-        randomNumber += this.props.userList.length;
-        return randomNumber;
-                                   }
+  generateID = (): number => {
+    let randomNumber: number = Math.floor(Math.random() * 1000);
+    randomNumber += this.props.userList.length;
+    return randomNumber;
+  }
 
-         newRegistration = (event:any) =>{
-         event.preventDefault();
-   // Handle retrieval of users field value.
-         const usersField: HTMLInputElement | null = document.querySelector( '[name="username"]' );
-         const passField: HTMLInputElement | null = document.querySelector( '[name="password"]' );
+  newRegistration = (event: any) => {
+    event.preventDefault();
+    // Handle retrieval of users field value.
+    const usersField: HTMLInputElement | null = document.querySelector('[name="username"]');
+    const passField: HTMLInputElement | null = document.querySelector('[name="password"]');
 
-         let usersFieldValue: string = '';
-         if ( usersField !== null ) usersFieldValue = usersField.value;
+    let usersFieldValue: string = '';
+    if (usersField !== null) usersFieldValue = usersField.value;
 
-          let passFieldValue: string = '';
-         if ( passField !== null ) passFieldValue = passField.value;
+    let passFieldValue: string = '';
+    if (passField !== null) passFieldValue = passField.value;
 
-         // Add new item to user.
-         this.props.signUp(usersFieldValue, passFieldValue);
-    
-           console.log (usersField, passField)
-            if ( usersField !== null ) usersField.value = '';
+    // Add new item to user.
+    this.props.signUp(usersFieldValue, passFieldValue);
 
-                if ( passField !== null ) passField.value = '';
-                                             }
+    console.log(usersField, passField)
+    if (usersField !== null) usersField.value = '';
 
-    render ()
-       {
+    if (passField !== null) passField.value = '';
+  }
+ 
+  render ()
+  {
 
-             let {userList} = this.props
-              console.log (userList);
-    return 
-    // eslint-disable-next-line no-unreachable
-    (
-        <Grid>
+    let {signUp} = this.props
+          console.log (signUp);
+    return (
 
-            <Grid.Column width={1}></Grid.Column>
-             <Grid.Column width={5}>
+      <Grid>
 
-            </Grid.Column>
-            <Grid.Column>      
+<Grid.Column width={1}></Grid.Column>
+<Grid.Column width={5}>
 
-            <Divider vertical></Divider>
-            </Grid.Column>
+</Grid.Column>
+<Grid.Column>      
 
-          <Grid.Column width={8}> 
-            <Header as='h2' color='blue' textAlign='left'>
-             Sign -Up
-            </Header>
-            <Form size='large' onSubmit ={this.newRegistration}>
-                <Form.Input name= 'username' fluid icon='user'  placeholder='UserName' />
-                <Form.Input name = 'password' fluid icon='lock' placeholder='Password' type='password'/>
+<Divider vertical></Divider>
+</Grid.Column>
+
+<Grid.Column width={8}> 
+<Header as='h2' color='blue' textAlign='left'>
+ Sign -Up
+</Header>
+<Form size='large' onSubmit ={this.newRegistration}>
+    <Form.Input name= 'username' fluid icon='user'  placeholder='UserName' />
+    <Form.Input name = 'password' fluid icon='lock' placeholder='Password' type='password'/>
       
-                <Button color='green' fluid size='large'>
-                SignUp
-                </Button>
-            </Form>
-          </Grid.Column>
+    <Button color='green' fluid size='large'>
+      SignUp
+    </Button>
+</Form>
+</Grid.Column>
 
 
-        </Grid>  
-    )
-      } 
-}
+</Grid>  
+    );
+    }
+  }
+  
 
 // Retrieve "users" from our "global" redux state.
-const mapStateToProps = ( state: RootState ) => {
+const mapStateToProps = (state: RootState) => {
   return {
     userList: state.user.userList
   }
-}
 
+}
 // Connect Redux and React using our values and "view!"
 export default connect(
   mapStateToProps,
   { signUp }
 )
-(Register);
+  (Register);
 
-    
+
