f0ff8cf0161b8a9169ced9447c05f7cd7016cb35
Zohairajweh <60945291+Zohairajweh@users.noreply.github.com>
2020-04-24 21:33:54 -0700

    Create NewsItemsList components and fuction for one user

--- src/components/NewsFeed/NewsItemsList.tsx
+++ src/components/NewsFeed/NewsItemsList.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react';
 import { Grid ,Image, Segment, Header, Divider} from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { RootState } from '../../store';
+import { NewsItem } from '../../store/news/types';
+import { signOut} from '../../store/user/actions';
 
+interface Routeparms{
+  id:string;
+}
 export interface INewsItemsListProps {
+  NewsItems:NewsItem[];
 }
 
-export default class NewsItemsList extends React.Component<INewsItemsListProps> {
+export  class NewsItemsList extends React.Component<INewsItemsListProps> {
+  constructor(props: INewsItemsListProps) {
+    super(props);
+    this.state = {newsId:'',postingUserId:'',newsTitle:'',newsContent:'',newsLikesCount:'',newsItemComment:""};
+  }
   public render() {
-
+    let { NewsItems } = this.props;
+    let newsTitle = NewsItems[0].newsTitle;
+    let newsContent = NewsItems[0].newsContent;
 
 
     return (
@@ -17,20 +31,18 @@ export default class NewsItemsList extends React.Component<INewsItemsListProps> 
         <Grid.Row verticalAlign='middle'>
           <Grid.Column>
           <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' size='medium' circular />
-
   
           </Grid.Column>
   
           <Grid.Column>
           <Header as='h1' color='blue' textAlign='center' size='huge'>
-        Header
+        {newsTitle}
                      </Header> 
              <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vestibulum elit in tortor finibus
-             </p>
+{newsContent
+}             </p>
           </Grid.Column>
         </Grid.Row>
-        
       </Grid>
       <Divider horizontal>.</Divider>
 
@@ -46,8 +58,7 @@ export default class NewsItemsList extends React.Component<INewsItemsListProps> 
   
           <Grid.Column>
           <Header as='h1' color='blue' textAlign='center' size='huge'>
-        Header
-                     </Header> 
+                             </Header> 
              <p>
                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vestibulum elit in tortor finibus
              </p>
@@ -62,3 +73,11 @@ export default class NewsItemsList extends React.Component<INewsItemsListProps> 
     );
   }
 }
+const mapStateToProps = (state: RootState) => {
+  return {
+    NewsItems:state.news.newsItemList,
+
+  };
+};
+
+export default connect(mapStateToProps,{signOut})(NewsItemsList);
