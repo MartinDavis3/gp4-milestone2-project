1d662103a5a2ffe5390325ee6a22513e5e1df710
Toyin <oluwatoyinogundare@gmail.com>
2020-04-25 01:48:24 -0600

    event handler created to add new comment

--- src/components/Savecomment.tsx
+++ src/components/Savecomment.tsx
@@ -1,4 +1,25 @@
 import React from 'react';
 import { RootState} from '../store';
 import {saveComment} from '../store/news/actions';
-import {SaveComment} from '../store/news/types';
+import {NewsItem} from '../store/news/types';
+import { Grid, Form, Input, Button, Card } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+
+export interface ICommentProps{
+    saveComment: typeof saveComment
+    newsItemList: NewsItem []
+}
+
+export class Comment extends React.Component<ICommentProps>
+{
+    generateID = (): number => {
+        let randomNumber: number = Math.floor( Math.random() * 1000 );
+        randomNumber += this.props.newsItemList.length;
+        return randomNumber;
+                               }
+
+     newComment = (event:any) =>{
+         event.preventDefault();
+     }
+
+}
