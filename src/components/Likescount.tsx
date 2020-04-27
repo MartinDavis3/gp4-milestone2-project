62b0f80d4cc20ef2c0c0962d6a7297e2c56a6bc9
Toyin <oluwatoyinogundare@gmail.com>
2020-04-27 15:05:34 -0600

    written generatelikes comment

--- src/components/Likescount.tsx
+++ src/components/Likescount.tsx
@@ -5,4 +5,39 @@ import {NewsItem} from '../store/news/types'
 import { Grid, Form, Input, Button, Card } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
+export interface ILikeProps
+{
+    incrementLikeCounter: typeof incrementLikeCounter,
+      newsItemList: NewsItem []
+}
 
+export class Like extends React.Component<ILikeProps>
+{
+    generateLikes = (): number => {
+    let randomNumber: number = Math.floor(Math.random() * 1000);
+    randomNumber += this.props.newsItemList.length;
+    return randomNumber;
+
+}
+
+ render ()
+ {
+     return (
+     <button>
+         <i className = "fas fa-heart"></i>
+     </button>
+ 
+     )}
+}
+
+const mapStateToProps = ( state: RootState ) => {
+  return {
+    newsItemList: state.news.newsItemList
+  }
+}
+
+// Connect Redux and React using our values and "view!"
+export default connect(
+  mapStateToProps,
+  { incrementLikeCounter }
+)( Like );
