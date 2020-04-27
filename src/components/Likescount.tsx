f7ebaa141ba9689440d5fd408a438a1b36d6c0de
Toyin <oluwatoyinogundare@gmail.com>
2020-04-27 17:06:39 -0600

    Add likecountComponent

--- src/components/Likescount.tsx
+++ src/components/Likescount.tsx
@@ -1,43 +1,41 @@
 import React from 'react';
-import {RootState} from '../store';
-import {incrementLikeCounter} from '../store/news/actions'
-import {NewsItem} from '../store/news/types'
-import { Grid, Form, Input, Button, Card } from 'semantic-ui-react';
+import { RootState } from '../store';
+import { incrementLikeCounter } from '../store/news/actions'
+import { NewsItem } from '../store/news/types'
 import { connect } from 'react-redux';
 
-export interface ILikeProps
-{
+export interface ILikescountProps {
     incrementLikeCounter: typeof incrementLikeCounter,
-      newsItemList: NewsItem []
+    newsItemList: NewsItem[],
+    currentItem: number
 }
 
-export class Like extends React.Component<ILikeProps>
-{
-    generateLikes = (): number => {
-    let randomNumber: number = Math.floor(Math.random() * 1000);
-    randomNumber += this.props.newsItemList.length;
-    return randomNumber;
-
-}
-
- render ()
- {
-     return (
-     <button>
-         <i className = "fas fa-heart"></i>
-     </button>
- 
-     )}
-}
-
-const mapStateToProps = ( state: RootState ) => {
-    return {
-      newsItemList: state.news.newsItemList
+export class Likescount extends React.Component<ILikescountProps>{
+    onLikeClick(): void {
+        this.props.incrementLikeCounter(this.props.currentItem);
     }
-  }
-  
-  // Connect Redux and React using our values and "view!"
-  export default connect(
-    mapStateToProps,
-    { incrementLikeCounter }
-  )( Like );
+
+
+
+    render() {
+        return (
+            <button onClick={() => this.onLikeClick()} >
+                <i className="fas fa-heart"></i>
+            </button>
+
+        )
+    }
+
+}
+
+    const mapStateToProps = (state: RootState) => {
+        return {
+            newsItemList: state.news.newsItemList
+        }
+    }
+
+        // Connect Redux and React using our values and "view!"
+        export default connect(
+            mapStateToProps,
+            { incrementLikeCounter }
+        )(Likescount);
